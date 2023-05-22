c83813014c331597cfcb7204fd3bf6e5c1dec2fd
Jun-Jie Zhu <shiroyuki@sjtu.edu.cn>
2023-05-22 22:02:25 +0800

    Update rmsd_calculation.py

--- Analysis/rmsd_calculation.py
+++ Analysis/rmsd_calculation.py
@@ -1,10 +1,11 @@
 import biotite.structure as struc
 import biotite.structure.io as strucio
 
-subject = '../StrucRef/predicted.150.47.pdb'
-reference = '../StrucRef/predicted.150.47.pdb'
+subject = '../StrucRef/predicted.5.35.pdb'
+reference = '../StrucRef/target.5.35.pdb'
 
 subject_array = strucio.load_structure(subject)
 reference_array = strucio.load_structure(reference)
+superimposed, _ = struc.superimpose(reference_array, subject_array)
 
-print(struc.rmsd(reference_array, subject_array))
+print(struc.rmsd(reference_array, superimposed))
