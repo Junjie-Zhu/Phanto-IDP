733ef2050ea0b260c6689794bbbd3bc45a91585d
Jun-Jie Zhu <shiroyuki@sjtu.edu.cn>
2023-06-02 18:18:33 +0800

    add pre-process scripts

---
+++ traj_process.sh
@@ -0,0 +1,8 @@
+path = ''
+
+python get_list.py ./formal/$path
+
+for i in `less pdb_list.dat`;
+    do awk -F " " '{if($3=="C"||$3=="N"||$3=="CA")print}' ./formal/$path/$i > ./processed/$path/$i;
+done
+sed -i "s/HIE/HIS/g" `grep -rl HIE ./processed/1a3s/`
