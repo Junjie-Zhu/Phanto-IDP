034f956017cc4f8dc37d45cc8c623a1c83d588e6
Jun-Jie Zhu <shiroyuki@sjtu.edu.cn>
2023-06-02 18:18:52 +0800

    Update traj_process.sh

--- traj_process.sh
+++ traj_process.sh
@@ -5,4 +5,4 @@ python get_list.py ./formal/$path
 for i in `less pdb_list.dat`;
     do awk -F " " '{if($3=="C"||$3=="N"||$3=="CA")print}' ./formal/$path/$i > ./processed/$path/$i;
 done
-sed -i "s/HIE/HIS/g" `grep -rl HIE ./processed/1a3s/`
+sed -i "s/HIE/HIS/g" `grep -rl HIE ./processed/$path/`
